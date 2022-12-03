d2e3e00f898ea330b8d98c30290ac2791f559f09
NataManatu <vip.aleksa78@mail.ru>
2022-12-03 22:19:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const toDo = {
+    data() {
+        return {
+            title: "Список заметок",
+            placeholderString: "Введите название заметки",
+            inputValue: "",
+            notes: ["заметка 1", "заметка 2"]
+        }
+    },
+    methods: {
+        inputChange(event) {
+            this.inputValue = event.target.value
+        },
+
+        addNewNote() {
+            if (this.inputValue !== "") {
+                this.notes.push(this.inputValue)
+                this.inputValue = ""
+            }
+
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(idx) {
+            this.notes.splice(idx, 1)
+        }
+
+
+    },
+    computed: {
+        dubleNoutes(){
+            return this.notes.length * 2
+        }
+
+    },
+    watch:{
+        inputValue(value){
+            console.log("input value ganged",value)
+        }
+    }
+}
+
+Vue.createApp(toDo).mount("#app")
